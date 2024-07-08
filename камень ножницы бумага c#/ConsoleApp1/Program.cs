5048fc5da4a3116a8f1023230e4f4bc35bdd0cfd
clonebidon <142823657+clonebidon@users.noreply.github.com>
2024-07-08 13:57:40 +0300

    Add files via upload

---
+++ камень ножницы бумага c#/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] choices = { "камень", "ножницы", "бумага" };
+
+        Console.WriteLine("Выберите: камень, ножницы или бумага:");
+        string userChoice = Console.ReadLine().ToLower();
+
+        Random random = new Random();
+        int randomIndex = random.Next(0, choices.Length);
+        string computerChoice = choices[randomIndex];
+
+        Console.WriteLine("Компьютер выбрал: " + computerChoice);
+
+        if (userChoice == computerChoice)
+        {
+            Console.WriteLine("Ничья!");
+        }
+        else if ((userChoice == "камень" && computerChoice == "ножницы") ||
+                 (userChoice == "ножницы" && computerChoice == "бумага") ||
+                 (userChoice == "бумага" && computerChoice == "камень"))
+        {
+            Console.WriteLine("Вы победили!");
+        }
+        else
+        {
+            Console.WriteLine("Вы проиграли!");
+        }
+    }
+}
